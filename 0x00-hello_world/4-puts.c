e0feb96db538131652c3c764e0126441b1d1da72
Abdulaye1 <abdulaye.matsoso@gmail.com>
2022-09-12 12:03:47 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,10 @@
 #include <stdio.h>
 
-/**                                                                                                                                     
-
-* main - Printing in C                                                                                                                  
-
-*                                                                                                                                       
-
-* Return: Always 0 (Success)                                                                                                            
-
-*/                                                                                                                                      
-
-int main(void)                                                                                                                          
-
-{                 
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+* main - Printing
+* Return: Always 0 (Successful)
+*/
+int main(void)
+{
+ puts("\"Programming is like building a multilingual puzzle");
+ return (0)
 }
